b92e8a33b56fabc343605083c625caa218762d59
Sanskar-Sakhareliya <sanskar.sakhareliya@bjtmail.com>
2023-12-16 11:25:03 +0530

    Minor changes for edit button

--- src/views/FormList.js
+++ src/views/FormList.js
@@ -53,7 +53,7 @@ export default function FormList() {
                                             <div>{item.name}</div>
                                             <div className='d-flex'>
                                                 <Link className='mx-1' to={`/attempt/${item._id}`}><Button className='btn-warning'>Attempt</Button> </Link>
-                                                <Link className='mx-2' to={`/attempt/${item._id}`}><Button className='btn-secondary '>
+                                                <Link className='mx-2' ><Button className='btn-secondary '>
                                                     <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><g fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"><path d="m16.475 5.408l2.117 2.117m-.756-3.982L12.109 9.27a2.118 2.118 0 0 0-.58 1.082L11 13l2.648-.53c.41-.082.786-.283 1.082-.579l5.727-5.727a1.853 1.853 0 1 0-2.621-2.621" /><path d="M19 15v3a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V7a2 2 0 0 1 2-2h3" /></g></svg>
                                                 </Button> </Link>
                                                 <Button onClick={() => deleteForm(item._id)} className='btn-danger d-flex justify-content-center align-items-center'>
